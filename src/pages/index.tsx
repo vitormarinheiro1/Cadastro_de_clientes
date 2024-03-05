c1bdb6cce23657842bd96450a6e7404612857c52
Suporte <suporte@HBR.LOCAL>
2024-03-05 13:48:59 -0300

    Projeto com hooks

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,54 +1,22 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
 import Layout from "@/components/Layout";
 import Tabela from "@/components/Tabela";
-import Cliente from "@/core/Cliente";
 import Botao from "@/components/Botao";
 import Formulario from "@/components/Formulario";
-import { useEffect, useState } from "react";
-import ClienteRepositorio from "@/core/ClienteRepositorio";
-import ColecaoCliente from "@/backend/db/ColecaoCliente";
+import useClientes from "@/hooks/useClientes";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
 
-  const repo: ClienteRepositorio = new ColecaoCliente()
-
-  const [cliente, setCliente] = useState<Cliente>(Cliente.vazio())
-  const [clientes, setClientes] = useState<Cliente[]>([])
-  const [visivel, setVisivel] = useState<'tabela' | 'form'>('tabela')
-
-  useEffect(obterTodos, [])
-
-  function obterTodos() {
-    repo.obterTodos().then(clientes => {
-      setClientes(clientes)
-      setVisivel('tabela')
-    })
-  }
-
-  function clienteSelecionado(cliente: Cliente) {
-    setCliente(cliente)
-    setVisivel('form')
-  }
-
-  async function clienteExcluido(cliente: Cliente) {
-    await repo.excluir(cliente)
-    obterTodos()
-  }
-
-  function novoCliente() {
-    setCliente(Cliente.vazio())
-    setVisivel('form')
-  }
-
-  async function salvarCliente(cliente: Cliente) {
-    await repo.salvar(cliente)
-    obterTodos()
-  }
-
-
+  const {
+    cliente,
+    clientes,
+    selecionarCliente,
+    excluirCliente,
+    salvarCliente,
+    novoCliente,
+    exibirTabela,
+    tabelaVisivel,
+  } = useClientes()
 
   return (
     <div className={`
@@ -56,20 +24,20 @@ export default function Home() {
     bg-gradient-to-r from-blue-500 to-purple-500
     `}>
       <Layout titulo="Cadastro de clientes">
-        {visivel === 'tabela' ? (
+        {tabelaVisivel ? (
           <>
             <div className="flex justify-end">
               <Botao className="mb-4" onClick={novoCliente}>Novo Cliente</Botao>
             </div>
             <Tabela clientes={clientes}
-              clienteSelecionado={clienteSelecionado}
-              clienteExcluido={clienteExcluido}
+              clienteSelecionado={selecionarCliente}
+              clienteExcluido={excluirCliente}
             />
           </>
         ) : (
           <Formulario cliente={cliente}
             clienteMudou={salvarCliente}
-            cancelado={() => setVisivel('tabela')}></Formulario>
+            cancelado={exibirTabela}></Formulario>
         )}
       </Layout>
     </div>
